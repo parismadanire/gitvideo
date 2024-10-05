d18f08ec8b5a737372f4f9b97e05b235cc24c4bd
parismadanire <parismadanire@gmail.com>
2024-10-05 17:20:46 -0400

    new file

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Hello CodeSandbox!")
+#Paris Madanire
+#I play tennis
+
+print('PPPP')
+print('P   P')
+print('P   P')
+print('PPPP')
+print('P')
+print('P')
+print('P')
+
+print('M   M')
+print('MM MM')
+print('MM MM')
+print('  M')
+print('M   M')
+print('M   M')
+print('M   M')
